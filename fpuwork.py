a5fa9707b533e457bcd4696443704bfc73147fe6
nathanhilbert <nathanhilbert@gmail.com>
2014-09-12 17:13:01 -0400

    half way through peopel vectorization

--- fpuwork.py
+++ fpuwork.py
@@ -8,6 +8,7 @@ from dateutil.parser import *
 from collections import OrderedDict
 from operator import itemgetter
 from datetime import datetime
+import numpy as np
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -15,6 +16,7 @@ from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.feature_extraction import DictVectorizer
 from scipy.cluster.vq import kmeans,vq
 from scipy.spatial import distance
+from sklearn.preprocessing import LabelEncoder
 
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -35,6 +37,7 @@ class Person():
         self.person_id = []
         self.fin_id = []
         self.DOB = None
+        self.age = None
         self.gender = None
         self.countryissue = []
 
@@ -47,6 +50,8 @@ class Person():
         self.suspicious = False
 
         self.verifiedfraud = False
+
+        self.vector = {}
 
     def get_or_create(self, lineobj):
         for person in personList:
@@ -69,6 +74,8 @@ class Person():
 
         try:
             self.DOB = parse(lineobj['DOB'].replace("-", "/"))
+            self.age = (datetime.datetime.now() - self.DOB).years
+            print self.age
         except:
             print lineobj['DOB'].replace("-", "/")
             print "failed DOB parse"
@@ -101,13 +108,13 @@ class Person():
                     #now that we have the object
                     if not lookback:
                         #create a new event
-                        lookback = Event(tempevent)
+                        lookback = Event(tempevent, self)
                         self.eventHistory.append(lookback)
                     elif (tempevent['EVENT_TYPE'] == "Departure"):
                         lookback.appendDeparture(tempevent)
                     else:
                         #create Event and save to lookback
-                        lookback = Event(tempevent)
+                        lookback = Event(tempevent, self)
                         self.eventHistory.append(lookback)
 
 
@@ -128,21 +135,49 @@ class Person():
         for histevent in self.eventHistory:
             if histevent.arrivalID and not histevent.departureID:
                 self.suspicious = True
+                histevent.suspicious = True
                 self.suspiciousRecord.append("Has arrival but not departure " +  str(histevent.arrivalID))
 
             if not histevent.arrivalID and histevent.departureID:
                 self.suspicious = True
+                histevent.suspicious = True
                 self.suspiciousRecord.append("Has depature but not arrival " +  str(histevent.departureID))
 
             if histevent.arrivalDate and histevent.departureDate:
-                if int((histevent.departureDate - histevent.arrivalDate).days) > 180:
+                numdays = histevent.calculateLengthofStay()
+                if numdays > 180:
                     self.suspicious = True
-                    self.suspiciousRecord.append("Has stayed longer than 180 days for" +  str(histevent.arrivalID) + " " + str(histevent.departureID))
+                    histevent.suspicious = True
+                    self.suspiciousRecord.append("Has stayed " + str(numdays) + " days for" +  str(histevent.arrivalID) + " " + str(histevent.departureID))
 
-            if (histevent.departureOverstay != "null" and int(histevent.departureOverstay) > 1) or (histevent.arrivalOverstay != "null" and int(histevent.arrivalOverstay) > 1):
+            if (histevent.departureOverstay not in  ("null",None) and int(histevent.departureOverstay) > 1) or (histevent.arrivalOverstay not in ("null",None) and int(histevent.arrivalOverstay) > 1):
                 self.suspicious = True
                 self.verifiedfraud = True
+                histevent.suspicious = True
+                histevent.verifiedfraud = True
                 self.suspiciousRecord.append("Has field of overstay " +  str(histevent.arrivalID) + " " +  str(histevent.departureID))
+
+    def getEvents(self):
+        return self.eventHistory
+
+    def pushVector(self,index,value):
+        self.vector[index] = value
+
+    def vectorize(self):
+        self.vector['numtrips'] = len(self.eventHistory)
+        lenofstay = []
+        numsuspciousevents = []
+        for event in self.eventHistory:
+            if event.lengthofstay != None:
+                lenofstay.append(event.lengthofstay)
+        self.vector['lengthofstay'] = np.mean(lenofstay)
+        #avg length of stay
+        #age
+        #num suspicious events
+
+
+
+
 
 
 
@@ -156,7 +191,8 @@ class Person():
 #an event is an arrival and depart date together
 class Event():
 
-    def __init__(self, lineobj):
+    def __init__(self, lineobj, person):
+        self.person = person
         self.docCountry = lineobj['DOCUMENT_COUNTRY_ISSUE']
         self.docType = lineobj['DOCUMENT_TYPE']
         self.LocationCode = lineobj['LOCATION_CODE']
@@ -165,16 +201,31 @@ class Event():
         self.carrierCode = lineobj['CARRIER_CODE']
         self.flightNum = lineobj['FLIGHT_NUMBER']
         self.psgStatus = lineobj['PSNGR_STATUS']
+        self.suspicious = False
+        self.verifiedfraud = False
+        self.vector = {}
         if lineobj['EVENT_TYPE'] == "Departure":
             self.departureDate = parse(lineobj['EVENT_DATE'])
             self.departureOverstay = lineobj['OVER_STAY_DAYS']
             self.departureID = lineobj['EVENT_ID']
+            try:
+                something = self.arrivalID
+            except:
+                self.arrivalDate = None
+                self.arrivalOverstay = None
+                self.arrivalID = None
         else:
             self.arrivalDate = parse(lineobj['EVENT_DATE'])
             self.arrivalOverstay = lineobj['OVER_STAY_DAYS']
             self.arrivalID = lineobj['EVENT_ID']
+            self.departureDate = None
+            self.departureOverstay = None
+            self.departureID = None
         self.claimForm = lineobj['CLAIMFORMTYPE']
-        self.i94Num = lineobj['I94_NUMBER']
+        if (lineobj['I94_NUMBER'] not in  ("null",0)):
+            self.i94Num = True
+        else:
+            self.i94Num = False
         self.reconCode = lineobj['RECONCILIATION_CODE']
         self.actionCode = lineobj['PRIMARY_ACTION_CODE']
         self.destAddress = lineobj['DEST_ADDRESS']
@@ -182,12 +233,128 @@ class Event():
         self.destState = lineobj['DEST_STATE_CODE']
         self.departAirport = lineobj['DEPARTURE_AIRPORT']
         self.travelMode = lineobj['TRAVEL_MODE']
+        self.lengthofstay = None
 
     def appendDeparture(self, lineobj):
         self.departureDate = parse(lineobj['EVENT_DATE'])
         self.departureOverstay = lineobj['OVER_STAY_DAYS']
         self.departureID = lineobj['EVENT_ID']
         return self
+
+    def calculateLengthofStay(self):
+        self.lengthofstay = int((self.departureDate - self.arrivalDate).days)
+        return self.lengthofstay
+
+    def pushVector(self,index,value):
+        self.vector[index] = value
+
+    def vectorize(self):
+        self.vector['i94Num'] = self.i94Num
+        self.vector['lengthofstay'] = self.lengthofstay
+
+
+masterEventsList  = []
+def getAllEvents(masterEventsList):
+    for person in personList:
+        masterEventsList += person.getEvents()
+    return masterEventsList
+
+
+
+###suspicious travel vs suspicious people
+
+
+genderEncoder = LabelEncoder()
+
+eventEncoders = {"docCountry": LabelEncoder(),\
+                 "docType": LabelEncoder(),\
+                 "LocationCode": LabelEncoder(),\
+                 "admissionClass": LabelEncoder(),\
+                 "carrierCode": LabelEncoder(),\
+                 "psgStatus": LabelEncoder(),\
+                 "destAddress": LabelEncoder(),\
+                 "destCity": LabelEncoder(),\
+                 "destState": LabelEncoder()
+                 }
+
+def buildEncodedLabels(masterEventsList):
+
+    #gender
+    temppersonobj = []
+    for person in personList:
+        temppersonobj.append(person.gender)
+
+    genderEncoder.fit_transform(temppersonobj)
+
+    masterEventsList = getAllEvents(masterEventsList)
+
+    for en in eventEncoders.keys():
+        templist = []
+        for tempevent in masterEventsList:
+            templist.append(getattr(tempevent,en))
+        eventEncoders[en].fit(templist)
+
+    return (genderEncoder, eventEncoders)
+
+
+
+
+#can now
+# >>> le.transform(["tokyo", "tokyo", "paris"])
+# array([2, 2, 1])
+# >>> list(le.inverse_transform([2, 2, 1]))
+
+
+
+def vectorizeEvents(masterEventsList):
+    for event in masterEventsList:
+        event.vectorize()
+
+    for en in eventEncoders.keys():
+        #get all event attributes
+        temparray = []
+        for event in masterEventsList:
+            temparray.append(getattr(event, en))
+        resultsset = eventEncoders[en].transform(temparray)
+        for theresult, event in zip(resultsset, masterEventsList):
+            event.pushVector(en, theresult)
+
+
+
+
+
+def vectorizePeople():
+    for person in personList:
+        person.vectorize()
+
+
+
+
+
+
+
+
+
+
+# >>> from sklearn.datasets import load_iris
+# >>> from sklearn.feature_selection import SelectKBest
+# >>> from sklearn.feature_selection import chi2
+# >>> iris = load_iris()
+# >>> X, y = iris.data, iris.target
+# >>> X.shape
+# (150, 4)
+# >>> X_new = SelectKBest(chi2, k=2).fit_transform(X, y)
+# >>> X_new.shape
+# (150, 2)
+
+#sklearn.feature_selection.univariate_selection.f_regression
+
+
+
+
+
+
+
 
 
 
@@ -225,42 +392,30 @@ for person in personList:
 
     person.buildOverstayCheck()
 
+
     #otyher checks
 
 
+genderEncoder, eventEncoders = buildEncodedLabels(masterEventsList)
+
+vectorizeEvents(masterEventsList)
+
+vectorizePeople()
 
 
+
+#check variables
+
+#apply model
+
+#SNA
+
+
+sys.exit(1)
 
 for person in personList:
     print person.vd_id
     print len(person.eventHistory)
-
-
-
-
-
-for line in ADFile:
-    numTot += 1
-    line_lower = line.strip().lower()
-    col = line.strip().split('\t')
-    if line_lower in seen:
-        numDup += 1
-    else:
-        seen.add(line_lower)
-    eventList.insert(len(eventList),col[21])
-    # DoB check since it's only month and year
-    dob = col[2].split('/')
-    if len(dob) == 2:
-        dobyear = dob[1]
-        dobmonth = dob[0]
-    # check ID & Person_ID_FK & FIN (null in Depart)
-    Person(col[0],col[1])
-    personList.add(Person)
-#DeDup the event List as IDs
-uniqueEvents = OrderedDict.fromkeys(eventList)
-print "Unique Events: ", len(uniqueEvents)
-print "Duplicates: ", numDup
-print "Total records read: ", numTot
 
 
 
